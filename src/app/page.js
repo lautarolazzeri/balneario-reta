ffbedbe50cab9778dc111d650f55eb8194246f6e
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-06-02 13:16:31 -0300

    info alquileres

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import '../../styles/globals.css'
 import Header from './containers/header'
 import Cards from './containers/cards'
+import Link from 'next/link'
 
 
 export default function Home() {
@@ -10,7 +11,22 @@ export default function Home() {
     <>
       <Header />
       <Cards />
-
+      <section>
+        <div className='alquileres-top'>
+          <div className='text-alquileres'>
+            <h1>Alquileres en Balneario Reta</h1>
+            <p>Conoce las mejores opciones de alquiler para tu estadía en Reta</p>
+          </div>
+          <Link href='#'>
+            <div className='button-alquileres'>
+              <p>Ver todos</p>
+              <div className='arrow-container'>
+                <Image src='/svg/arrow.svg' width={20} height={20} />
+              </div>
+            </div>
+          </Link>
+        </div>
+      </section>
     </>
   )
 }
