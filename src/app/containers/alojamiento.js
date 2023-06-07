de1336db6d654e6adb64247982b6f26a636b7438
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-06-07 20:33:56 -0300

    Update

--- src/app/containers/alojamiento.js
+++ src/app/containers/alojamiento.js
@@ -6,7 +6,7 @@ import GradientSection from "../components/gradient-section";
 function Alojamientos() {
   return (
     <section className='alquileres'>
-      <GradientSection title='Alquileres en Balneario Reta' subtitle='Conoce las mejores opciones de alquiler para tu estadía en Reta' url='#' />
+      <GradientSection title='Cabañas en alquiler en Balneario Reta' subtitle='Conoce las mejores opciones de alquiler para tu estadía en Reta' url='#' />
       <div className='alquileres-container'>
         <SliderMaxWidth />
       </div>
