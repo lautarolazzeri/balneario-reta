f325fac116e2a143883ee5b0c400595dd5d66f37
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-06-19 19:48:52 -0300

    update

--- src/app/containers/cards.js
+++ src/app/containers/cards.js
@@ -3,10 +3,10 @@ import SingleCard from "../components/single-card";
 function Cards() {
   return <div className='cards'>
     <SingleCard image='./svg/alojamientos.svg' title='Alojamientos' text='Encuentra los mejores alquileres en Reta' url='/#alojamientos' />
-    <SingleCard image='./svg/turismo.svg' title='Turismo' text='Lugares para visitar cerca de Balneario Reta' url='/#tourism' />
-    <SingleCard image='./svg/info.svg' title='Información' text='Encuentra la información mas actualizada de Reta' url='/#info' />
-    <SingleCard image='./svg/services.svg' title='Servicios' text='Los mejores servicios de Reta se encuentran aquí' url='/#services' />
-    <SingleCard image='./svg/images.svg' title='Imágenes de Reta' text='Las mejores fotos de Balneario Reta' url='/#images' />
+    <SingleCard image='./svg/turismo.svg' title='Turismo' text='Lugares para visitar cerca de Balneario Reta' url='/#turismo' />
+    <SingleCard image='./svg/info.svg' title='Información' text='Encuentra la información mas actualizada de Reta' url='/#informacion' />
+    <SingleCard image='./svg/services.svg' title='Servicios' text='Los mejores servicios de Reta se encuentran aquí' url='/#servicios' />
+    <SingleCard image='./svg/images.svg' title='Imágenes de Reta' text='Las mejores fotos de Balneario Reta' url='/#imagenes' />
   </div>
 }
 
