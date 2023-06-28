94c767855b834e562cda7e4cfd6d9831e4a123da
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-06-28 14:13:01 -0300

    wbnow

--- src/app/containers/imagenes.js
+++ src/app/containers/imagenes.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 
-async function Imagenes() {
+function Imagenes() {
   return (
     <div className="images" id="imagenes">
       <div className="text-alquileres">
