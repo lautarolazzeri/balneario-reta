c5ae9b4dfc400d2f12de307d2e5a093a60f447b4
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-06-29 16:06:55 -0300

    UPDATE

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,8 @@ export default function RootLayout({ children }) {
         <title>{metadata.title}</title>
 
 
-
+        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7215423654996673"
+          crossorigin="anonymous"></Script>
         <meta name="google" content="nositelinkssearchbox" />
         <meta name="google" content="notranslate" />
       </head>
