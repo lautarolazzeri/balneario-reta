286373bb1aa7724f44697dc582a2381a56184577
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-07-04 17:34:39 -0300

    delete icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
         <meta name="google" content="notranslate" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/lg.png" sizes="16x16" />
 
 
 
