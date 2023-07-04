97f159c3685799e156a985f6b0a05138c14c1da0
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-07-04 17:49:33 -0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,19 +17,7 @@ export default function RootLayout({ children }) {
         <meta name="google" content="notranslate" />
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link
-          rel="icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
 
-        <link
-          rel="apple-touch-icon"
-          href="/icon?<generated>"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@BalnearioRetaOficial" />
