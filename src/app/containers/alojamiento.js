3c535a5a965d4f85715bf2f90b1511dca6a63c5c
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-07-24 21:02:49 -0300

    alquileres

--- src/app/containers/alojamiento.js
+++ src/app/containers/alojamiento.js
@@ -6,7 +6,7 @@ import GradientSection from "../components/gradient-section";
 function Alojamientos() {
   return (
     <section className='alquileres' id="alojamientos">
-      <GradientSection title='Cabañas en alquiler en Reta, Tres Arroyos' subtitle='Conoce las mejores opciones de alquiler para tu estadía en Reta' url='/alojamientos' />
+      <GradientSection title='Alquileres en Reta, Tres Arroyos' subtitle='Conoce las mejores opciones de alojamientos para tu estadía en Reta' url='/alojamientos' />
       <div className='alquileres-container'>
         <SliderMaxWidth />
       </div>
