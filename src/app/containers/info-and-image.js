20bcc48dd33126be5dab5053ee49801723104bbb
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-07-26 19:39:46 -0300

    info and image

--- src/app/containers/info-and-image.js
+++ src/app/containers/info-and-image.js
@@ -8,9 +8,9 @@ function InfoAndImage() {
           <Image src='/reta-img/amanecer-reta.webp' alt="amanecer en Reta" width={500} height={500} />
         </div>
         <div className='container-text'>
-          <h2>Explora la belleza de Balneario Reta</h2>
+          <h2>Explora la belleza de estas playas</h2>
           <p>
-            Sumérgete en la serenidad de Balneario Reta, un paraíso costero en la costa argentina. Disfruta de sus playas de arena dorada, impresionantes atardeceres y emocionantes actividades acuáticas. Escapa del bullicio urbano y conecta con la belleza natural deL pueblo, es tu refugio perfecto donde podrás relajarte, explorar y crear recuerdos inolvidables. Ven y experimenta la serenidad y la belleza natural de Balneario Reta, un destino perfecto para escapar y rejuvenecer.
+            Sumérgete en la serenidad de Reta, un paraíso costero en la costa argentina. Disfruta de sus playas de arena dorada, impresionantes atardeceres y emocionantes actividades acuáticas. Escapa del bullicio urbano y conecta con la belleza natural deL pueblo, es tu refugio perfecto donde podrás relajarte, explorar y crear recuerdos inolvidables. Ven y experimenta la serenidad y la belleza natural de este balneario, un destino perfecto para escapar y rejuvenecer.
           </p>
         </div>
       </div>
