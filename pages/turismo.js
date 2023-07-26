6d743211bbbe60b2d120d9d9afaeb91099d81b80
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-07-26 19:31:29 -0300

    turismo

--- pages/turismo.js
+++ pages/turismo.js
@@ -25,7 +25,7 @@ export default function Turismo() {
         <meta name="twitter:image" content="https://balnearioreta.vercel.app/reta-img/imagen-reta%20(51).webp" />
 
         <meta name="title" content="Balneario Reta Oficial - Conoce los mejores lugares para visitar en Balnerio Reta y alrededores" />
-        <meta name="description" content="Página oficial de Balneario Reta, tu guía turistica de Reta. Cabañas en alquiler, atracciones turísticas, información de reta, eventos y muchas imágenes" />
+        <meta name="description" content="Página oficial de Balneario Reta, tu guía turistica de Reta. Visita los lugares turisticos que tiene para ofrecer Balneario Reta y sus alrededores, consulta los lugares en google maps" />
         <meta name="keywords" content="balneario reta, tres arroyos, festivales, eventos, cabañas en reta, turismo reta, imagenes de reta, reta, alojamientos,Cabañas Marsu, Alojamientos Turismo Información, alquileres en reta, servicios, Reta contacto, imagenes, cabañas, mapa, playas, costa Buenos Aires" />
         <meta name="robots" content="index, follow" />
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -34,7 +34,7 @@ export default function Turismo() {
         <meta name="author" content="Lautaro Sebastián Lazzeri" />
 
         <meta property="og:title" content="balneario reta oficial - Conoce los mejores lugares para visitar en Balnerio Reta y alrededores" />
-        <meta property="og:description" content="Descubre la página web de Balneario Reta, tu guía completa para conocer todo sobre Reta. Explora alojamientos, atracciones turísticas, información interesante, eventos, festivales y muchas imágenes." />
+        <meta property="og:description" content="Página oficial de Balneario Reta, tu guía turistica de Reta. Visita los lugares turisticos que tiene para ofrecer Balneario Reta y sus alrededores, consulta los lugares en google maps" />
         <meta property="og:locale" content="es_AR" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://balnearioreta.vercel.app" />
@@ -51,7 +51,7 @@ export default function Turismo() {
         <OtherNav />
         <div className="tourism-page" >
           <div className="top-tourism-page">
-            <h1>Cuáles son las atracciones turísticas más elegidas por los turistas en Reta {currentYear}</h1>
+          <h1>Turismo en Balneario reta - actividades y lugares {currentYear}</h1>
           <p>Embárcate en una aventura inolvidable en Reta y descubre las atracciones turísticas más populares. Desde sus playas doradas hasta sus impresionantes paisajes naturales, sumérgete en la belleza y diversión que ofrece este balneario. Planifica tu visita y disfruta de experiencias inolvidables en este destino de ensueño en {currentYear}.</p>
           </div>
 
