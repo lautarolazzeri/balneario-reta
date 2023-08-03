58b13a980feb4ad30c17ba11ac50893651916662
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2023-08-02 23:59:13 -0300

    updaee

--- src/app/components/slide-item.js
+++ src/app/components/slide-item.js
@@ -7,20 +7,20 @@ function SliderItem(props) {
     <swiper-slide>
       <div className='alquileres-card'>
         <div className='alquileres-card-image'>
-          <Link href={url}><Image placeholder="/svg/blur.svg" alt={title} src={image} width={300} height={200} /></Link>
+          <Link target="_blank" href={url}><Image placeholder="/svg/blur.svg" alt={title} src={image} width={300} height={200} /></Link>
         </div>
         <div className='alquileres-card-text'>
-          <Link href={url}><h3>{title}</h3></Link>
+          <Link target="_blank" href={url}><h3>{title}</h3></Link>
           <p className='cant-personas'>{people}</p>
           <p className='direction'>{location}</p>
           <div className='contact'>
-            <Link href={wsp}>
+            <Link target="_blank" href={wsp}>
               <div className='whatsapp-contact'>
                 <Image placeholder="/svg/blur.svg" alt='Whatsapp logo' src='/svg/whatsapp.svg' width={20} height={20} />
                 <p>Whatsapp</p>
               </div>
             </Link>
-            <Link href={tel}>
+            <Link target="_blank" href={tel}>
               <div className='phone-contact'>
                 <Image placeholder="/svg/blur.svg" src='/svg/blackphone.svg' alt="Phone logo" width={20} height={20} />
                 <p>Llamar</p>
